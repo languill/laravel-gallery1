5db513523e8fa1c799316a0dc61cb4bfffd366ba
languill <eco.telly@gmail.com>
2021-08-12 16:55:26 +0400

    first commit

---
+++ resources/views/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <img src="/{{$imageInView->image}}" alt="" class="img-thumbnail gallery-image">
+        </div>
+    </div>
+</div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+
+Route::get('/', 'ImagesController@index');
+
+Route::get('/about', 'HomeController@about');
+
+Route::get('/create', 'ImagesController@create');
+
+Route::post('/store', 'ImagesController@store');
+
+Route::get('/show/{id}', 'ImagesController@show');
+
+Route::get('/edit/{id}', 'ImagesController@edit');
+
+Route::post('/update/{id}', 'ImagesController@update');
+
+Route::get('/delete/{id}', 'ImagesController@delete');
